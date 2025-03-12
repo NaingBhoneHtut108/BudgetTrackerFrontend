f15b5d4988241b742268eaf67f650054dcc7e15c
NaingBhoneHtut108 <naingbhonehtut108@gmail.com>
2025-03-12 16:37:03 +0630

    Connect To Backend

--- src/app/auth/signup/signup/signup.component.ts
+++ src/app/auth/signup/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-signup',
@@ -15,16 +16,37 @@ export class SignupComponent {
   username : string = "";
   email : string = "";
   password : string = "";
+  apiUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient, private router : Router){
+    this.testAPI();
+  }
 
+  testAPI(){
+    console.log("Calling API")
+    this.http.get(this.apiUrl + '/test').subscribe(
+      data=> {
+        console.log(JSON.stringify(data));
+      },
+      error=>{
+        console.log("Failed To Call API : " + JSON.stringify(error));
+      }
+    )
   }
 
   signUp(){
     console.log("Username : " + this.username);
     console.log("Email : " + this.email);
     console.log("Password : " + this.password);
-    
+    var requestData : any  = {'username' : this.username, 'email' : this.email, 'password' : this.password};
+    // this.http.post(this.apiUrl + '/user/singup',requestData).subscribe(
+    //   data => {
+    //     console.log("Signed Up Successful");
+    //   },
+    //   error => {
+    //     // Handle Error
+    //   }
+    // )
     // TODO : call API
   }
 
